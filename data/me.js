d49ad3678e9e7a009b39ba4715ffb0dac67992c4
JdM <julien@rmbi.ch>
2023-05-09 20:45:05 +0200

    new questions

--- data/me.js
+++ data/me.js
@@ -2,5 +2,8 @@ const me = {
 "who are you": "👋 Hi! I am JdM or Judemont. I'm 14 years old and I love programming awesome projects to <b>improve myself</b> 🚀.",
 "what programming languages do you know": "💻 I know mostly Python and JavaScript but I also do PHP, TypeScript, HTML, and CSS.",
 "what project are you currently working on": "🔨 Currently, I am working a lot on a new open-source social network: <a href='https://github.com/asterjdm/cesco'>Cesco</a>. But I also work on a lot of other projects, you can see them on <a href='https://github.com/judemont'>my GitHub</a> 🌟",
+"which operating system do you use": "I use ubuntu (linux)🐧🐧.",
+"which text editor do you use": "I use Visual Studio Code.",
+"why did you make this site like this": "Because I want to.",
 "how can i contact you": "📞 You can contact me with: <br> GitHub : <a href='https://github.com/judemont'>github.com/judemont</a> <br> Telegram : <a href='https://t.me/judemont'>t.me/judemont</a> <br> Email : <a href='mailto:julien@rmbi.ch?subject=Hi JdM !'>julien@rmbi.ch</a>"
 }
