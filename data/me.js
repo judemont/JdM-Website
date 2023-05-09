43641b1c6189e983ac4d0e008e2a6035d5dac8e4
JdM <julien@rmbi.ch>
2023-05-09 19:29:43 +0200

    I-i

--- data/me.js
+++ data/me.js
@@ -2,5 +2,6 @@ const me = {
     "who are you": "Hi! I am JdM or Judemont.<br> I'm 14 years old and I love programming awesome projects to <b>improve myself</b>.",
     "what programming languages do you know": "I know mostly python and javascript but I also do php, typescript, html and css.",
     "what project are you currently working on": "Currently I am working a lot on a new open source social network : <a href='https://github.com/asterjdm/cesco'>Cesco</a><br> But I also work on a lot of other projects, you can see them on <a href='https://github.com/judemont'>my github</a>",
-    "how can I contact you": "You can contact me with: <br> Github : <a href='https://github.com/judemont'>github.com/judemont</a> <br> Telegram : <a href='https://t.me/judemont'>t.me/judemont</a> <br> Email : <a href='mailto:julien@rmbi.ch>julien@rmbi.ch</a> " 
+    "how can i contact you": "You can contact me with: <br> Github : <a href='https://github.com/judemont'>github.com/judemont</a> <br> Telegram : <a href='https://t.me/judemont'>t.me/judemont</a> <br> Email : <a href='mailto:julien@rmbi.ch>julien@rmbi.ch</a> " ,
+    "test": "test"
 }
