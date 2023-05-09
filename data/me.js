8aa4edbd1ee6a0edc75b42bf20cb69108320434d
JdM <julien@rmbi.ch>
2023-05-09 19:20:00 +0200

    Add more questions and answers

--- data/me.js
+++ data/me.js
@@ -1,3 +1,5 @@
 const me = {
-    "who are you": "Hi, I am JdM or Judemont !<br> I'm 14 years old and I love programming awesome projects to improve myself."
+    "who are you": "Hi! I am JdM or Judemont.<br> I'm 14 years old and I love programming awesome projects to <b>improve myself</b>.",
+    "what programming languages do you know": "I know mostly python and javascript but I also do php, typescript, html and css.",
+    "what project are you currently working on": "Currently I am working a lot on a new open source social network : <a href='https://github.com/asterjdm/cesco'>Cesco</a><br> But I also work on a lot of other projects, you can see them on <a href='https://github.com/judemont'>my github</a>"
 }
