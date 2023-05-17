2b94547394f8b8de6ac3ca332c1d5314fce5cf3a
JdM <julien@rmbi.ch>
2023-05-17 12:55:30 +0200

    fullscreen fixed

--- script/useFullscreen.js
+++ script/useFullscreen.js
@@ -1,12 +1,17 @@
-function openFullscreen() {
-    let elem = document.documentElement;
-    if (elem.requestFullscreen) {
-      elem.requestFullscreen();
-    } else if (elem.webkitRequestFullscreen) { /* Safari */
-      elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) { /* IE11 */
-      elem.msRequestFullscreen();
-    }
-}
 
-openFullscreen();
+
+function openFullScreen() {
+    let elem = document.documentElement;
+  
+    if (!document.fullscreenElement) {
+      elem.requestFullscreen().catch((err) => {
+        alert(
+          `Error attempting to enable fullscreen mode: ${err.message} (${err.name})`
+        );
+      });
+    } else {
+      document.exitFullscreen();
+    }
+  }
+
+openFullScreen();
