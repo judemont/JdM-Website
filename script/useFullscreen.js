c782e03d6a4c6fad779f7db1e196ee7974d10fd4
JdM <julien@rmbi.ch>
2023-05-17 12:52:56 +0200

    USE FULLSCREEN !!

---
+++ script/useFullscreen.js
@@ -0,0 +1,12 @@
+function openFullscreen() {
+    let elem = document.documentElement;
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+      elem.msRequestFullscreen();
+    }
+}
+
+openFullscreen();
