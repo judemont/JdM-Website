9a1fa7f11e425662e0bb9a433893a742d7ceb24b
JdM <julien@rmbi.ch>
2023-07-04 13:08:39 +0200

    ITsentences

---
+++ script.js
@@ -0,0 +1,15 @@
+const description = document.getElementById('description');
+
+const ITsentences = [
+    "Program or be programmed.",
+    "Simplicity is the soul of efficiency.",
+    "Hello, World!",
+    "Eat, Sleep, Code, Repeat.",
+    "In code we trust.",
+    "Keep calm and code on.",
+    "There are 10 types of people in the world: Those who understand binary and those who don't.",
+    "Open source is not just about sharing code, it's about sharing knowledge and empowering communities.",
+    "Code never lies, comments sometimes do.",
+    "Programming is the future we're building.",
+    "Linux is not just an operating system, it's a philosophy."
+]
