d57b44ffcb253bc0a02c676eb16f7f0dd105536f
JdM <julien@rmbi.ch>
2023-07-04 17:30:02 +0200

    remove unused div

--- infinityScroll.js
+++ infinityScroll.js
@@ -1,9 +1,9 @@
 var height = 2000
 window.addEventListener("scroll", function() {
-    let infinitSpace = document.getElementById("body");
+    let body = document.getElementById("body");
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
     let heightToAdd = height + scrollTop + 300
-    infinitSpace.style.height = heightToAdd + "px";
+    body.style.height = heightToAdd + "px";
 });
   
