8c6a48c0ebbc59237867e6e182025363063c5462
JdM <julien@rmbi.ch>
2023-07-04 15:04:14 +0200

    created infinite scrolly

--- infinityScroll.js
+++ infinityScroll.js
@@ -1,5 +1,9 @@
-var space = document.createElement('div');
+var height = 0
+window.addEventListener("scroll", function() {
+    let infinitSpace = document.getElementById("infinitSpace");
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-if (window.scrollY){
-
-}
+    let heightToAdd = height + scrollTop + 100
+    infinitSpace.style.height = heightToAdd + "px";
+});
+  
