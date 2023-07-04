7647bdb10e530b80d80d461027e4820f47963605
JdM <julien@rmbi.ch>
2023-07-04 18:56:00 +0200

    u

--- infinityScroll.js
+++ infinityScroll.js
@@ -1,14 +1,9 @@
 var defaultHeight = 2000
-var defaultBackSize = 105
+
 
 window.addEventListener("scroll", function() {
     let body = document.getElementById("body");
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if(scrollTop < 1210){
-        body.style.backgroundSize = defaultBackSize + scrollTop/20 + "%"
-    }else{
-        body.style.backgroundSize = (defaultBackSize - scrollTop) + "%"
-    }
 
     let heightToAdd = defaultHeight + scrollTop + 300
     body.style.height = heightToAdd + "px";
