a8f4f109bac584234f2418685a8d3a96235c3f2a
JdM <julien@rmbi.ch>
2023-07-04 13:29:02 +0200

    update sentances .-,

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const description = document.getElementById('description');
-
+const changeInterval = 1000
 const ITsentences = [
     "Program or be programmed.",
     "Simplicity is the soul of efficiency.",
@@ -11,5 +11,21 @@ const ITsentences = [
     "Open source is not just about sharing code, it's about sharing knowledge and empowering communities.",
     "Code never lies, comments sometimes do.",
     "Programming is the future we're building.",
-    "Linux is not just an operating system, it's a philosophy."
+    "Linux is not just an operating system, it's a philosophy.",
+    "Code better every day, be better every day."
 ]
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+
+
+
+
+setInterval(function(){
+    let rand = getRandomInt(ITsentences.length);
+    place.innerText = ITsentences[rand];
+
+}, changeInterval, ITsentences, description)
