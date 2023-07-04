02fef79c799b6e5be6cd71b4f37de89ff609b5cb
JdM <julien@rmbi.ch>
2023-07-04 17:39:58 +0200

    zoom background on scroll

--- infinityScroll.js
+++ infinityScroll.js
@@ -1,9 +1,12 @@
-var height = 2000
+var defaultHeight = 2000
+var defaultBackSize = 105
 window.addEventListener("scroll", function() {
     let body = document.getElementById("body");
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-    let heightToAdd = height + scrollTop + 300
+    body.style.backgroundSize = defaultBackSize + scrollTop + "%"
+
+    let heightToAdd = defaultHeight + scrollTop + 300
     body.style.height = heightToAdd + "px";
 });
   
