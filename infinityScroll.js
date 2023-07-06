0759a41ea05a9478a467db96db67002d26ee2b52
JdM <julien@rmbi.ch>
2023-07-06 19:44:00 +0200

    change background

--- infinityScroll.js
+++ infinityScroll.js
@@ -3,7 +3,7 @@ var defaultHeight = 2000
 
 window.addEventListener("scroll", function() {
     let body = document.getElementById("body");
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
     let heightToAdd = defaultHeight + scrollTop + 300
     body.style.height = heightToAdd + "px";
