1d9029bc80dc3db4b69d52475b38f727f2788367
JdM <julien@rmbi.ch>
2023-08-23 19:38:02 +0200

    created the title animation

--- script/titleAnimation.js
+++ script/titleAnimation.js
@@ -1,8 +1,14 @@
-const myText = new SplitType('#mainTitle')
-        
-gsap.to(".char",{
-    y:0,
-    stagger: 0.05,
-    delay:0.2,
-    duration: .1,
-})
+const myText = new SplitType('#mainTitle');
+
+gsap.to(".char", {
+    y: 0,
+    stagger: 1,
+    delay: 1,
+    onComplete: () => {
+        gsap.to("#mainTitle", {
+            rotation: 720, // Angle de rotation en degrés
+            duration: 1,   // Durée de l'animation de rotation
+            ease: "power2.inOut" // Type d'interpolation pour l'animation
+        });
+    }
+});
