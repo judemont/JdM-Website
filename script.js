4c0ee21e474ec6d3c853aef630257f6ede36f482
JdM <julien@rmbi.ch>
2024-01-28 20:52:55 +0100

    updated

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const helloWorldH1 = document.getElementById("helloWorld")
-const helloWorlds = ["Hello World", "Bonjour le monde", "مرحباً بالعالم", "你好，世界", "Hej verden", "Tere maailmast", "Hallo Welt", "Γεια σου κόσμε", "Ciao mondo", "ハロー・ワールド", "헬로 월드", "Здравствуй мир", "Hola Mundo", "Привіт, світ."]
+const helloWorldH1 = document.getElementById("helloWorld");
+const helloWorlds = ["Hello World", "Bonjour le monde", "مرحباً بالعالم", "你好，世界", "Hej verden", "Tere maailmast", "Hallo Welt", "Γεια σου κόσμε", "Ciao mondo", "ハロー・ワールド", "헬로 월드", "Здравствуй мир", "Hola Mundo", "Привіт, світ."];
 
-setInterval(function () {
-    const random = Math.floor(Math.random() * helloWorlds.length)
-    helloWorldH1.textContent = helloWorlds[random]
-}, 1000)
+let interval = setInterval(function() {
+    let counter = 0;
+    const langinterval = setInterval(function () {
+        counter++;
+
+        const random = Math.floor(Math.random() * helloWorlds.length);
+        helloWorldH1.textContent = helloWorlds[random];
+
+        if (counter >= 10) {
+            helloWorldH1.textContent = helloWorlds[0]
+            clearInterval(langinterval);
+        }
+    }, 90);
+    clearInterval(interval);
+}, 5000);
