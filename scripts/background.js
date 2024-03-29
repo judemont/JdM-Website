5e7048d599dad37854f3adda0f9d583c17a5362d
JdM <julien@rmbi.ch>
2024-03-29 13:56:28 +0100

    more backgrounds

--- scripts/background.js
+++ scripts/background.js
@@ -2,7 +2,7 @@
 
 const backgroundVideo = document.getElementById("backgroundVideo")
 const backgroundVideoSource = document.getElementById("backgroundVideoSource")
-const backgroundNum = Math.round(Math.random() * 3)
+const backgroundNum = Math.round(Math.random() * 7)
 
 backgroundVideoSource.src = `images/background${backgroundNum}.mp4`
 backgroundVideo.load()
