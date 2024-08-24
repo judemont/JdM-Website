232c5560037453a2feec9239ff2029c3cecabbf2
judemont <julien@rmbi.ch>
2024-08-24 18:16:20 +0200

    Refactor: Update index.html layout and styles

--- scripts/background.js
+++
@@ -1,8 +0,0 @@
-
-
-const backgroundVideo = document.getElementById("backgroundVideo")
-const backgroundVideoSource = document.getElementById("backgroundVideoSource")
-const backgroundNum = Math.round(Math.random() * 7)
-
-backgroundVideoSource.src = `images/background${backgroundNum}.mp4`
-backgroundVideo.load()
